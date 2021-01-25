ba9c65b710aff7c5b313757765487eff2ed8fd73
Aravindan007 <53030761+Aravindan007@users.noreply.github.com>
2021-01-25 09:01:56 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainchartComponent } from './dashboard/mainchart/mainchart.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    DashboardComponent,
+    MainchartComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
